21f5440c402074f1afae83db5512999faa3ffa79
jadonhoang <jadonhoang@gmail.com>
2024-02-28 19:23:04 -0800

    basic routing

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { MessagesComponent } from './messages/messages.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { 'path': '', component: HomeComponent },
+    { 'path': 'contact', component: ContactComponent }, 
+    { 'path': 'messages', component: MessagesComponent },
+    { 'path': 'login', component: LoginComponent }
+
+];
